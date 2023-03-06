3a1927c372a7c52f0e607ada5498a534f7b42fb5
d-lynch95 <darraghlynch1995@gmail.com>
2023-03-06 05:03:24 +0000

    add tofixed method to sundaypay formula to correct output to 2 decimal places

--- assets/javascript/calculations.js
+++ assets/javascript/calculations.js
@@ -8,37 +8,37 @@ let sunday = document.getElementById("sunday-hours");
 
 function mondayPay() {
     let result = monday.value * weekday_pay;
-    return document.getElementById("mon-pay").innerHTML = result;
+    return document.getElementById("mon-pay").innerHTML = result.toFixed(2);
 }
 
 function tuesdayPay() {
     let result = tuesday.value * weekday_pay;
-    return document.getElementById("tue-pay").innerHTML = result;
+    return document.getElementById("tue-pay").innerHTML = result.toFixed(2);
 }
 
 function wednesdayPay() {
     let result = wednesday.value * weekday_pay;
-    return document.getElementById("wed-pay").innerHTML = result;
+    return document.getElementById("wed-pay").innerHTML = result.toFixed(2);
 }
 
 function thursdayPay() {
     let result = thursday.value * weekday_pay;
-    return document.getElementById("thur-pay").innerHTML = result;
+    return document.getElementById("thur-pay").innerHTML = result.toFixed(2);
 }
 
 function fridayPay() {
     let result = friday.value * weekday_pay;
-    return document.getElementById("fri-pay").innerHTML = result;
+    return document.getElementById("fri-pay").innerHTML = result.toFixed(2);
 }
 
 function satudayPay() {
     let result = saturday.value * saturday_pay;
-    return document.getElementById("sat-pay").innerHTML = result;
+    return document.getElementById("sat-pay").innerHTML = result.toFixed(2);
 }
 
 function sundayPay() {
     let result = sunday.value * sunday_pay;
-    return document.getElementById("sun-pay").innerHTML = result;
+    return document.getElementById("sun-pay").innerHTML = result.toFixed(2);
 }
 
 
